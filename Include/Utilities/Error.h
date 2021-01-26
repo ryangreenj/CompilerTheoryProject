81ee8eb142ca9c43239655a5f12aacdacd986c1c
Ryan <ryangreen105+siemens@gmail.com>
2021-01-26 09:46:28 -0500

    Re-structure files again

---
+++ Include/Utilities/Error.h
@@ -0,0 +1,17 @@
+#ifndef _INCL_UTILITIES_ERROR
+#define _INCL_UTILITIES_ERROR
+
+typedef unsigned int ERROR_TYPE;
+
+#define RET_IF_ERR(error) if (error != ERROR_NONE) return error;
+
+#define ERROR_NONE 0
+#define ERROR_FAIL_TO_OPEN 1
+#define ERROR_END_OF_FILE 2
+
+namespace Error
+{
+
+}
+
+#endif
